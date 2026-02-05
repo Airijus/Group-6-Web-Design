a36db0eb8388b119b6eac60a0e501f37dd416df3
DawidWladecki <dw756@student.aru.ac.uk>
2026-02-05 15:00:15 +0000

    Add files via upload

---
+++ Group 6 Website/script.js
@@ -0,0 +1,7 @@
+const myButton = document.getElementById("myButton");
+const myDiv = document.getElementById("nav");
+
+myButton.addEventListener("click", event => {
+
+    myDiv.style.display = "none";
+})
