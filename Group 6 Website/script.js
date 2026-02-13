bbbfe3a6cc45893f2e7ffd1bfadbec579529e7f0
Thomas <TFG104@student.aru.ac.uk>
2026-02-13 15:39:26 +0000

    - Uploaded the basic build from Teams. - Using page one to develop my lesson page. - Updated page one to GSCE Percentages and added very basic introduction and tested embedding a wordsearch. - Altered the CSS (mainly padding/margins) and added extra for page one. - The .txt are for the EDEXCEL information I need and a place to log WordSearchLabs word search names and passwords.

--- Group 6 Website/script.js
+++
@@ -1,7 +0,0 @@
-const myButton = document.getElementById("myButton");
-const myDiv = document.getElementById("nav");
-
-myButton.addEventListener("click", event => {
-
-    myDiv.style.display = "none";
-})
